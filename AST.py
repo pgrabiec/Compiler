69ed165ded8b88239725c1e7b1a389f667f3a94e
Wojciech Baczyński <baczynskiwojtek@gmail.com>
2016-11-28 22:58:53 +0100

    Revert "Finally modified AST.py and Cparser.py"
    This reverts commit 7e1db0756440e02068ae1286c9a047bcefd0dcb0.

--- AST.py
+++ AST.py
@@ -14,7 +14,7 @@ class Program(Node):
 
 
 class Segments(Node):
-    def __init__(self, line=None, segment=None):
+    def __init__(self, line, segment=None):
         """:arg segment : AST.Segment"""
         super().__init__(line)
         self.segments = []
@@ -102,11 +102,11 @@ class WhileInstruction(Node):
 
 
 class RepeatInstruction(Node):
-    def __init__(self, line, instructions, condition):
+    def __init__(self, line, condition, instructions):
         """:arg instructions : AST.Instructions"""
         super().__init__(line)
-        self.instructions = instructions
         self.condition = condition
+        self.instructions = instructions
 
 
 class ReturnInstruction(Node):
@@ -131,7 +131,7 @@ class CompoundInstruction(Node):
 
 
 class CompoundSegments(Node):
-    def __init__(self, line=None, segment=None):
+    def __init__(self, line, segment):
         """:arg segment : AST.CompoundSegment"""
         super().__init__(line)
         self.segments = []
@@ -172,13 +172,13 @@ class String(Const):
 
 
 class BinExpr(Node):
-    def __init__(self, line, left, op, right):
-        """:arg left : AST.Expression
-           :arg op : string
+    def __init__(self, line, op, left, right):
+        """:arg op : string
+           :arg left : AST.Expression
            :arg right : AST.Expression"""
         super().__init__(line)
-        self.left = left
         self.op = op
+        self.left = left
         self.right = right
 
 
@@ -192,7 +192,7 @@ class FunctionCallExpression(Node):
 
 
 class ExpressionList(Node):
-    def __init__(self, line=None, expression=None):
+    def __init__(self, line, expression):
         """:arg expression : AST.Expression"""
         super().__init__(line)
         self.expressions = []
@@ -213,7 +213,7 @@ class FunctionDefinition(Node):
 
 
 class ArgumentsList(Node):
-    def __init__(self, line=None, argument=None):
+    def __init__(self, line, argument):
         """:arg argument : AST.Argument"""
         super().__init__(line)
         self.arguments = []
