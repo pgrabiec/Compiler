97c0c9d0094cb17c724b00e18d6a9323d95c740b
Piotr <pgrabiec7@gmail.com>
2016-11-29 03:54:37 +0100

    Another work done

--- semantics/TypeChecker.py
+++ semantics/TypeChecker.py
@@ -31,7 +31,7 @@ class TypeChecker(NodeVisitor):
     def __init__(self):
         super().__init__()
         self.ttype = {}
-        self.in_loop = False                    # For detecting errors with break; and continue;
+        self.in_loop = False  # For detecting errors with break; and continue;
         self.return_statement_occurred = False  # For detecting missing return statement
         self.init_ttype()
         self.scope_manager = ScopeManager()
@@ -94,12 +94,12 @@ class TypeChecker(NodeVisitor):
             expression = self.visit(init.expression)
             if self.scope_manager.seek_symbol(identifier) is None:  # No symbol in the scope
                 self.scope_manager.add_scope_symbol(identifier, AST.Variable(init.line, identifier, type))  # add symbol
-            else:   # Symbol already present in scope
+            else:  # Symbol already present in scope
                 self.error(init.line, "Declaration: id \'%s\' is already defined" % identifier)
-            try:    # Verify initialization value
+            try:  # Verify initialization value
                 if self.ttype["="][type, expression] != type:
                     raise KeyError  # return value type is not compatible with 'type'
-            except KeyError:    # types mismatch
+            except KeyError:  # types mismatch
                 self.error(init.line, "Declaration: attempt to initialize \'%s\' variable \'%s\' with \'%s\' value" %
                            (type, identifier, expression))
 
@@ -120,56 +120,83 @@ class TypeChecker(NodeVisitor):
     def visit_Assignment(self, node):
         identifier = node.variable.identifier
         expression = self.visit(node.expression)
-        if self.scope_manager.seek_symbol(identifier) is None:
+        variable = self.scope_manager.seek_symbol(identifier)
+        if variable is None:
             self.error(node, "Assignment: variable \'%s\' undeclared" % identifier)
-        else: # TODO
+        else:
+            var_type = variable.type
+            try:
+                if self.ttype["="][var_type, expression] != var_type:
+                    raise KeyError
+            except KeyError:
+                self.error(node, "Assignment: cannot assign value of type \'%s\' to variable \'%s\' type \'%s\'" %
+                           (expression, identifier, var_type))
 
     def visit_ChoiceInstruction(self, node):
-        pass
+        self.visit(node.condition)
+        self.visit(node.instruction_true)
+        if node.instruction_false is not None:
+            self.visit(node.instruction_false)
 
     def visit_WhileInstruction(self, node):
-        pass
+        self.in_loop = True
+        self.visit(node.condition)
+        self.visit(node.instruction)
+        self.in_loop = False
 
     def visit_RepeatInstruction(self, node):
+        self.in_loop = True
+        self.visit(node.instructions)
+        self.visit(node.condition)
+        self.in_loop = False
+
+    def visit_BreakInstruction(self, node):
+        if not self.in_loop:
+            self.error(node, "Break: break instruction not inside a loop")
+
+    def visit_ContinueInstruction(self, node):
+        if not self.in_loop:
+            self.error(node, "Continue: continue instruction not inside a loop")
+
+    def visit_CompoundInstruction(self, node):
+        self.visit(node.instructions)
+
+    def visit_CompoundSegments(self, node):
+        for segment in node.segments:
+            self.visit(segment)
+
+    def visit_CompoundSegment(self, node):
+        self.visit(node.content)
+
+    def visit_Condition(self, node):
+        self.visit(node.expression)
+
+    def visit_Const(self, node):
+        return self.visit(node.value)
+
+    def visit_Integer(self, node):
+        return "int"
+
+    def visit_Float(self, node):
+        return "float"
+
+    def visit_String(self, node):
+        return "string"
+
+    def visit_BinExpr(self, node):
+        left = self.visit(node.left)
+        right = self.visit(node.right)
+        op = node.op
+        try:
+            return self.ttype[op][left, right]
+        except KeyError:
+            self.error(node, "Binary Expression: cannot apply \'%s\' to \'%s\' and \'%s\'" % (op, left, right))
+
+    # ["fun id"][(ret_type, [Variable(id, type), ...])]
+    def visit_FunctionCallExpression(self, node):
         pass
 
     def visit_ReturnInstruction(self, node):
-        pass
-
-    def visit_BreakInstruction(self, node):
-        pass
-
-    def visit_ContinueInstruction(self, node):
-        pass
-
-    def visit_CompoundInstruction(self, node):
-        pass
-
-    def visit_CompoundSegments(self, node):
-        pass
-
-    def visit_CompoundSegment(self, node):
-        pass
-
-    def visit_Condition(self, node):
-        pass
-
-    def visit_Const(self, node):
-        pass
-
-    def visit_Integer(self, node):
-        pass
-
-    def visit_Float(self, node):
-        pass
-
-    def visit_String(self, node):
-        pass
-
-    def visit_BinExpr(self, node):
-        pass
-
-    def visit_FunctionCallExpression(self, node):
         pass
 
     def visit_ExpressionList(self, node):
