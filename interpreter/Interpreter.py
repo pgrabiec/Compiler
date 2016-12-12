f127a130c60fc60a94405685a9db55374d5d345b
Piotr <pgrabiec7@gmail.com>
2016-12-12 22:25:24 +0100

    Implemented Memory and created templates for Interpreter visit methods

--- interpreter/Interpreter.py
+++ interpreter/Interpreter.py
@@ -14,35 +14,94 @@ class Interpreter(object):
     def visit(self, node):
         pass
 
-    @when(AST.BinOp)
+    @when(AST.Node)
     def visit(self, node):
-        r1 = node.left.accept(self)
-        r2 = node.right.accept(self)
-        # try sth smarter than:
-        # if(node.op=='+') return r1+r2
-        # elsif(node.op=='-') ...
-        # but do not use python eval
+        pass
 
-    @when(AST.RelOp)
+    @when(AST.Program)
     def visit(self, node):
-        r1 = node.left.accept(self)
-        r2 = node.right.accept(self)
-        # ...
+        pass
+
+    @when(AST.Declaration)
+    def visit(self, node):
+        pass
+
+    @when(AST.Init)
+    def visit(self, node):
+        pass
+
+    @when(AST.PrintInstruction)
+    def visit(self, node):
+        pass
+
+    @when(AST.LabeledInstruction)
+    def visit(self, node):
+        pass
 
     @when(AST.Assignment)
     def visit(self, node):
-        ...
-        #
-        #
+        pass
+
+    @when(AST.ChoiceInstruction)
+    def visit(self, node):
+        pass
+
+    @when(AST.WhileInstruction)
+    def visit(self, node):
+        pass
+
+    @when(AST.RepeatInstruction)
+    def visit(self, node):
+        pass
+
+    @when(AST.ReturnInstruction)
+    def visit(self, node):
+        pass
+
+    @when(AST.BreakInstruction)
+    def visit(self, node):
+        pass
+
+    @when(AST.ContinueInstruction)
+    def visit(self, node):
+        pass
 
     @when(AST.Const)
     def visit(self, node):
-        return node.value
+        pass
 
-    # simplistic while loop interpretation
-    @when(AST.WhileInstr)
+    @when(AST.Integer)
+    def visit(selfself, node):
+        pass
+
+    @when(AST.Float)
     def visit(self, node):
-        r = None
-        while node.cond.accept(self):
-            r = node.body.accept(self)
-        return r
+        pass
+
+    @when(AST.String)
+    def visit(self, node):
+        pass
+
+    @when(AST.Identifier)
+    def visit(self, node):
+        pass
+
+    @when(AST.FunctionCallExpression)
+    def visit(self, node):
+        pass
+
+    @when(AST.BinExpr)
+    def visit(self, node):
+        pass
+
+    @when(AST.FunctionDefinition)
+    def visit(self, node):
+        pass
+
+    @when(AST.Argument)
+    def visit(self, node):
+        pass
+
+    @when(AST.Variable)
+    def visit(self, node):
+        pass
--- interpreter/Memory.py
+++ interpreter/Memory.py
@@ -1,32 +1,39 @@
 class Memory:
     def __init__(self, name):  # memory name
-        ...
+        self.name = name
+        self.variables = {}
 
     def has_key(self, name):  # variable name
-        ...
+        return name in self.variables
 
     def get(self, name):  # gets from memory current value of variable <name>
-        ...
+        return self.variables[name]
 
     def put(self, name, value):  # puts into memory current value of variable <name>
-        ...
+        self.variables[name] = value
 
 
 class MemoryStack:
     def __init__(self, memory=None):  # initialize memory stack with memory <memory>
-        ...
+        self.mem_stack = []
+        if memory is not None:
+            self.mem_stack.append(memory)
 
     def get(self, name):  # gets from memory stack current value of variable <name>
-        ...
+        size = len(self.mem_stack)
+        memory = self.mem_stack[size - 1]
+        return memory.get(name)
 
     def insert(self, name, value):  # inserts into memory stack variable <name> with value <value>
-        ...
+        self.set(name, value)
 
     def set(self, name, value):  # sets variable <name> to value <value>
-        ...
+        size = len(self.mem_stack)
+        memory = self.mem_stack[size - 1]
+        memory.put(name, value)
 
     def push(self, memory):  # pushes memory <memory> onto the stack
-        ...
+        self.mem_stack.append(memory)
 
     def pop(self):  # pops the top memory from the stack
-        ...
+        self.mem_stack.pop()
